f3a1c413fa14a5f2515d58a7715573509928af18
mohsen-parsa <mohsen.parsa313@gmail.com>
2013-05-14 18:25:45 -0700

    fentask controllers updated

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,5 +1,7 @@
 package controllers;
 
+import models.Project;
+import models.Task;
 import play.*;
 import play.mvc.*;
 
@@ -8,7 +10,8 @@ import views.html.*;
 public class Application extends Controller {
   
     public static Result index() {
-        return ok(index.render("Your new application is ready."));
+        return ok(index.render( Project.find.all(), Task.find.all()
+        )); 
     }
   
 }
